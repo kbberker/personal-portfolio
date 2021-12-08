61d8eca9da46e2a50c13a2b45f51434f9f52af7e
kbberker <kbbuk90@gmail.com>
2021-12-08 15:28:56 +0000

    feat: add blogs and handling of blogs

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,15 @@
+
+const { DateTime } = require("luxon")
+
+
 module.exports = function(eleventyConfig) {
 
   // by default 11ty doesn't take in CSS
   eleventyConfig.addPassthroughCopy('./src/index.css')
+
+  eleventyConfig.addFilter("formatDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+  })
 
   return {
     dir: {
