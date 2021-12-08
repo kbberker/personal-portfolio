ea223ba20151505e3dbf0dd314407233ba568127
kbberker <kbbuk90@gmail.com>
2021-12-08 16:26:38 +0000

    chore: fixed admin yml

--- .eleventy.js
+++ .eleventy.js
@@ -6,6 +6,7 @@ module.exports = function(eleventyConfig) {
 
   // by default 11ty doesn't take in CSS
   eleventyConfig.addPassthroughCopy('./src/index.css')
+  eleventyConfig.addPassthroughCopy('./src/admin')
 
   eleventyConfig.addFilter("formatDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
